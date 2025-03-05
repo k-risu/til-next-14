9b370b2bd9eed397ead637972983e0df07cde6e3
k-risu <kkd4869@gmail.com>
2025-03-05 13:08:04 +0900

    SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { fetchGoods } from "@/lib/fetch-good";
 import { fetchRandomGood } from "@/lib/fetch-random-good";
 import styles from "@/pages/index.module.css";
 import { InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import { ReactNode } from "react";
 
 // Next 에는 약속이 된 함수가 있다.
@@ -28,22 +29,31 @@ export default function Home({
   randomGoods,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={styles.container}>
-      <section>
-        <h3>지금 추천하는 상품</h3>
-        {/* 3개만 랜덤하게 출력 */}
-        {randomGoods.map((item) => (
-          <GoodItem key={item.id} {...item} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 상품</h3>
-        {/* 전체 상품 출력 */}
-        {allGoods.map((item) => (
-          <GoodItem key={item.id} {...item} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>해외 쇼핑몰 추천 서비스</title>
+        <meta name="discription" content="해외 상품 추천서비스입니다." />
+        <meta property="og:title" content="해외 쇼핑몰 추천 서비스" />
+        <meta property="og:discription" content="해외 상품 추천서비스입니다." />
+        <meta property="og:image" content="/thumbnail.png" />
+      </Head>
+      <div className={styles.container}>
+        <section>
+          <h3>지금 추천하는 상품</h3>
+          {/* 3개만 랜덤하게 출력 */}
+          {randomGoods.map((item) => (
+            <GoodItem key={item.id} {...item} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 상품</h3>
+          {/* 전체 상품 출력 */}
+          {allGoods.map((item) => (
+            <GoodItem key={item.id} {...item} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -7,29 +7,53 @@ import SearchLayout from "@/components/search-layout";
 import { ReactNode, useEffect, useState } from "react";
 import GoodDataType from "@/types";
 import { fetchSearchGood } from "@/lib/fetch-search-good";
+import Head from "next/head";
 
 export default function Page() {
   const [goods, setGoods] = useState<GoodDataType[]>([]);
+
   const router = useRouter();
   const { keyword } = router.query;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchSearchGood(keyword as string);
+    setGoods(data);
+  };
+
   useEffect(() => {
-    const fetchSearchResult = async () => {
-      const data = await fetchSearchGood(keyword as string);
-      setGoods(data);
-    };
+    // 키워드가 바뀌면 실행한다.
     fetchSearchResult();
   }, [keyword]);
+
   return (
-    <div className={styles.container}>
-      <h4>
-        <strong>{keyword}</strong> : 검색 결과
-      </h4>
-      <div>
-        {goods.map((item) => (
-          <GoodItem key={item.id} {...item} />
-        ))}
+    <>
+      <Head>
+        <title>해외 쇼핑몰 {keyword} 검색 서비스</title>
+        <meta
+          name="discription"
+          content={`해외 상품 ${keyword} 검색 서비스입니다.`}
+        />
+        <meta
+          property="og:title"
+          content={`해외 쇼핑몰 ${keyword} 검색 서비스`}
+        />
+        <meta
+          property="og:discription"
+          content={`해외 상품 ${keyword} 검색 서비스입니다.`}
+        />
+        <meta property="og:image" content="/thumbnail.png" />
+      </Head>
+      <div className={styles.container}>
+        <h4>
+          <strong>{keyword}</strong> : 검색 결과
+        </h4>
+        <div>
+          {goods.map((item) => (
+            <GoodItem key={item.id} {...item} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
